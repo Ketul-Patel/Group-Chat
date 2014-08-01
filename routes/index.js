dd1160643fb5abd4da018d77e27ed79973abc4d2
Ketul Patel <kjpatel2010@gmail.com>
2014-08-01 14:14:41 -0700

    added basic functionality

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
-var express = require('express');
-var router = express.Router();
+module.exports = function Route(app){
+	var users = {};
+	var messages = [];
+	app.get('/', function(req, res) {
+		res.render('main');
+	})
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
-});
+	app.io.route('got_a_new_user', function(req) {
+		req.io.emit('user_setup', {name: req.data.name, users: users, messages: messages});
+		users[req.sessionID] = {name: req.data.name};
+		var message = "<p>"+req.data.name+" has joined the chat.</p>"
+		messages.push(message);
+		req.io.broadcast('new_user', {name: req.data.name, message: message});
+	})
+	app.io.route('new_message', function(req) {
+		var message = "<p>"+req.data.name+" says: "+req.data.message+"</p>"
+		messages.push(message);
+		app.io.broadcast('display_message', {message: message});
+	})
 
-module.exports = router;
+	app.io.route('disconnect', function(req) {
+		req.io.broadcast('user_disconnected', {id: req.sessionID})
+		delete users[req.sessionID]
+	})
+}
